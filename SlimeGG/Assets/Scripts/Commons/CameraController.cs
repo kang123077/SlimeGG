47499b14ed343045eb08f198d80566180cda1705
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-12-04 01:10:32 +0900

    add(Camera moving)
    #1
    + Scene 기본 구성
    + Camera 이동 스크립트 구성

---
+++ SlimeGG/Assets/Scripts/Commons/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float speed_move = 5.0f;
+    private float speed_wheel = 3000.0f;
+    private new Transform transform;
+    private new Camera camera;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCameraByKeyBoard();
+    }
+
+    void moveCameraByKeyBoard()
+    {
+        float keyH = Input.GetAxis("Horizontal");
+        float keyV = Input.GetAxis("Vertical");
+        float keyD = Input.GetAxis("Mouse ScrollWheel");
+        keyH = keyH * speed_move * Time.deltaTime;
+        keyV = keyV * speed_move * Time.deltaTime;
+        keyD = keyD * speed_wheel * Time.deltaTime;
+        transform.Translate(Vector3.right * keyH);
+        transform.Translate(Vector3.up * keyV);
+        if (
+            (transform.position.z <= 3 && transform.position.z >= -3)
+            || (transform.position.z <= 3 && keyD >= 0)
+            || (transform.position.z >= -3 && keyD <= 0)
+        )
+            transform.Translate(Vector3.forward * keyD);
+    }
+}
