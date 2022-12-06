a372363301df63f35aa845c44de5dd8a404d5f3d
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-12-07 01:36:19 +0900

    add(소켓 맵 자동 생성)
    #1
    
    + 소켓 생성용 스크립트
    ~ 타일 이벤트 처리를 위한 z 좌표 분리: 10 -> 9
    ~ 화면 관점 Orthogrpahic 으로 변경
    + Tag 추가: tile, Socket

---
+++ SlimeGG/Assets/Scripts/Maps/SocketGenerator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocketGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject socketBase;
+    private Transform[] Lines = new Transform[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject newLine = new GameObject();
+            newLine.name = "Line " + i;
+            Transform newLineTr = newLine.transform;
+            newLineTr.position = new Vector3(
+                transform.position.x + i % 2 == 0 ? 0 : 1,
+                transform.position.y - (i * 2),
+                transform.position.z
+            );
+            newLineTr.SetParent(transform);
+            for (int j = 0; j < 6; j++)
+            {
+                GameObject newSocket = Instantiate(socketBase);
+                newSocket.name = "Socket " + j;
+                newSocket.tag = "Socket";
+                Transform newSocketTr = newSocket.transform;
+                newSocketTr.position = newLineTr.position;
+                newSocketTr.position = new Vector3(
+                    newLineTr.position.x + (j * 2),
+                    newLineTr.position.y,
+                    newLineTr.position.z
+                );
+                newSocketTr.SetParent(newLineTr);
+            }
+        }
+    }
+}
