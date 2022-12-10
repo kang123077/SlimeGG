965ceeac9b124653a42fd55e334f87a70f05653f
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-12-10 15:58:55 +0900

    add(TileSet Generator)
    #1
    
    + 타일셋 제너레이터 기본 구성

--- SlimeGG/Assets/Scripts/Maps/TileSetInventoryController.cs
+++ SlimeGG/Assets/Scripts/Maps/TileSetInventoryController.cs
@@ -5,7 +5,14 @@ using UnityEngine.EventSystems;
 
 public class TileSetInventoryController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private Transform contentParent;
     private bool isMouseIn = false;
+    private Vector2 margin = new Vector2(100f, -40f);
+
+    void Start()
+    {
+        contentParent = transform.Find("Viewport").Find("Content");
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         isMouseIn = true;
@@ -20,4 +27,28 @@ public class TileSetInventoryController : MonoBehaviour, IPointerEnterHandler, I
     {
         return isMouseIn;
     }
+
+    public void addTileSet(Transform newTileSet)
+    {
+        newTileSet.GetComponent<TileSetController>().setIsInInventory(true);
+        newTileSet.SetParent(transform.Find("Viewport").Find("Content"));
+        newTileSet.localScale = new Vector3(40f, 40f, 1f);
+    }
+
+    public void removeTileSet(Transform targetTileSet)
+    {
+        targetTileSet.GetComponent<TileSetController>().setIsInInventory(false);
+        targetTileSet.SetParent(null);
+        targetTileSet.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+    //public void sortInventory()
+    //{
+    //    for (int i = 0; i < contentParent.childCount; i++)
+    //    {
+    //        Transform curTileSet = contentParent.GetChild(i);
+    //        Vector3 tileSetSize = curTileSet.GetComponent<TileSetController>().getSize();
+    //        curTileSet.position = new Vector3()
+    //    }
+    //}
 }
