5b0e4d7248f5aeeab5bc3b235b73ecd96ca14ec5
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-12-12 21:10:15 +0900

    Monster composing (#4)
    * add(Tile Type)
    
    #3
    
    + 타일의 속성
    + 테스트용 이미지 파일들
    
    * feat(Applying Types)
    
    #3
    
    + 임시 타일에 속성 가시화
    
    * add(Drawer For Monster)
    
    #3
    
    + 몬스터 Prefab
    + 몬스터 생성기
    + 설치된 타일셋 보관용 오브젝트 구성
    + 최초 고정용 타일 구성
    
    * feat(Monster and TileSet)
    
    #3
    
    + 드래그 & 드롭 몬스터 이동
    + 몬스터 타일셋에 포함
    + 몬스터 포함 시, 타일셋 이동 불가
    
    * feat(TilseSet Collider)
    
    #3
    
    ~ 타일셋 콜라이더 수정 -> Composite Collider
    
    * feat(Monster assignment by mouse)
    
    #3
    
    ~ Composite Collider 2D 대응용 RayCast 2D로 변경
    
    * feat(Monster collider 2d)
    
    #3
    
    ~ 몬스터 Collider & Rigid Body 2D로 변경
    
    * feat(Monster and TileSet)
    
    #3
    
    ~ 몬스터 타일셋 사이 로직 추가 구성 -> Layer 이용
    
    * feat(Monster and TileSet)
    
    #3
    
    ~ 몬스터 최초 생성 시 고정 0,0 타일셋에 생성
    ~ 타일셋 Composite Collider 처리:: 몬스터 O <- Outlines, X <- Polygon
    
    * feat(Monster and TileSet)
    
    #3
    
    ~ 타일셋 이동 시, 몬스터와 충돌 시 물리 이벤트 layer 통해서 배제

---
+++ SlimeGG/Assets/Scripts/Monsters/Generators/MonsterGenerator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject monsterBase;
+    private MonsterInfo[] monsterInfos;
+    public GameObject baseTileSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void generateMonsters()
+    {
+        monsterInfos = new MonsterInfo[1];
+        for (int i = 0; i < monsterInfos.Length; i++)
+        {
+            generateMonster(monsterInfos[i]);
+        }
+    }
+
+    private void generateMonster(MonsterInfo monsterInfo)
+    {
+        GameObject newMonster = Instantiate(monsterBase);
+        newMonster.GetComponent<MonsterBaseController>().assignMonsterToTileSet(baseTileSet.transform);
+        newMonster.transform.localPosition = new Vector3(0f, 0f, 0f);
+    }
+}
