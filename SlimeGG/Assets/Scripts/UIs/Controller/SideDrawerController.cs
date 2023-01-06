32c4906b3f30e5fb8291771acc52e0f13f903dde
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-07 01:35:20 +0900

    [Add] Drawers for monster info
    #38
    
    + 전투 전 몬스터 사전 정보를 위한 드로어 UI 구성

---
+++ SlimeGG/Assets/Scripts/UIs/Controller/SideDrawerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SideDrawerController : MonoBehaviour
+{
+    private Transform toggleButtonTf;
+    private bool isOpen = true;
+    private bool isArrived = true;
+    [SerializeField]
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleButtonTf = transform.Find("Toggle Button");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 3, transform.GetComponent<RectTransform>().sizeDelta.y);
+        toggleButtonTf.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 3 / 10, Screen.width / 3 / 10 * 2);
+        if (isOpen)
+        {
+            transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            transform.GetComponent<RectTransform>().anchoredPosition = new Vector3((isRight ? 1f : -1f) * Screen.width / 3, 0f, 0f);
+        }
+    }
+
+    public void toggleDrawer()
+    {
+        if (isOpen)
+        {
+            transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width * 5 / 6, 0f, 0f);
+            isOpen = false;
+            isArrived = false;
+        }
+        else
+        {
+            transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width / 2, 0f, 0f);
+            isOpen = true;
+            isArrived = false;
+        }
+    }
+}
