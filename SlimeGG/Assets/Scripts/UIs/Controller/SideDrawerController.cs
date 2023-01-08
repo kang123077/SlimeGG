bef3f4a74809c02dd9582d82e485adb1249f366d
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-08 16:16:43 +0900

    [Feat] Vertical Drawer
    #44
    
    ~ 화면 테두리 서랍 UI 수직 타입 추가

--- SlimeGG/Assets/Scripts/UIs/Controller/SideDrawerController.cs
+++ SlimeGG/Assets/Scripts/UIs/Controller/SideDrawerController.cs
@@ -7,11 +7,12 @@ public class SideDrawerController : MonoBehaviour
 {
     private Transform toggleButtonTf;
     private bool isOpen = false;
-    private bool isArrived = true;
     [SerializeField]
-    public bool isRight;
+    public bool isRight = false;
     [SerializeField]
-    public bool isHeightFull;
+    public bool isFull;
+    [SerializeField]
+    public bool isVertical = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,21 @@ public class SideDrawerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 3, isHeightFull ? transform.GetComponent<RectTransform>().sizeDelta.y : Screen.height / 5);
+        if (isVertical)
+        {
+            transform.GetComponent<RectTransform>().sizeDelta = new Vector2(isFull ? 0 : 100f, Screen.height / 2);
+            toggleButtonTf.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 10, Screen.height / 2 / 5);
+            if (isOpen)
+            {
+                transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, 0f, 0f);
+            }
+            else
+            {
+                transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, (isVertical ? -1f : -1f) * Screen.height / 2, 0f);
+            }
+            return;
+        }
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 3, isFull ? transform.GetComponent<RectTransform>().sizeDelta.y : Screen.height / 5);
         toggleButtonTf.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 3 / 10, Screen.width / 3 / 10 * 2);
         if (isOpen)
         {
@@ -38,15 +53,27 @@ public class SideDrawerController : MonoBehaviour
     {
         if (isOpen)
         {
-            transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width * 5 / 6, 0f, 0f);
+            if (isVertical)
+            {
+                transform.localPosition = new Vector3(0f, (isVertical ? 1f : -1f) * Screen.width * 5 / 12, 0f);
+            }
+            else
+            {
+                transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width * 5 / 12, 0f, 0f);
+            }
             isOpen = false;
-            isArrived = false;
         }
         else
         {
-            transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width / 2, 0f, 0f);
+            if (isVertical)
+            {
+                transform.localPosition = new Vector3(0f, (isVertical ? 1f : -1f) * Screen.width / 4, 0f);
+            }
+            else
+            {
+                transform.localPosition = new Vector3((isRight ? 1f : -1f) * Screen.width / 2, 0f, 0f);
+            }
             isOpen = true;
-            isArrived = false;
         }
     }
 }
