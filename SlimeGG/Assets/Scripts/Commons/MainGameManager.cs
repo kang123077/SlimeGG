583f40fcf11d14d88e4e040c38c012ccc0635faa
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-27 17:17:35 +0900

    Add: loading Screen
    #49
    
    + 페이드인/아웃 스크린
    + 게임 시작 시, 씬 이동

--- SlimeGG/Assets/Scripts/Commons/MainGameManager.cs
+++ SlimeGG/Assets/Scripts/Commons/MainGameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainGameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject loadingGO;
     // Start is called before the first frame update
     void Start()
     {
-
+        controllLoading(false, null);
     }
 
     // Update is called once per frame
@@ -23,5 +27,43 @@ public class MainGameManager : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    private void controllLoading(bool isStart, string targetSceneName)
+    {
+        StartCoroutine(fadeScreen(isStart, targetSceneName));
+    }
+
+    public void startGame()
+    {
+        controllLoading(true, "BattleScene");
+    }
+
+    public void endGame()
+    {
+        controllLoading(false, null);
+    }
+
+    private IEnumerator fadeScreen(bool isStart, string targetSceneName)
+    {
+        if (isStart)
+        {
+            loadingGO.SetActive(isStart);
+        }
+        float cnt = 0f;
+        while (cnt < 1f)
+        {
+            cnt += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            loadingGO.GetComponent<Image>().color = new Color(0f, 0f, 0f, isStart ? cnt : (1f - cnt));
+        }
+        if (targetSceneName != null)
+        {
+            SceneManager.LoadScene(targetSceneName);
+        }
+        if (!isStart)
+        {
+            loadingGO.SetActive(isStart);
+        }
     }
 }
