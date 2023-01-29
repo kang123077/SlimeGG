e9bdb9e61e6f3fd770a7aba6b4240d4f9fb56546
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-29 21:43:44 +0900

    Add: initial camera movement
    #50
    
    + 씬 로드 시, 현재 스테이지 위치로 카메라 smooth 이동

--- SlimeGG/Assets/Scripts/Dungeons/Managers/DungeonManager.cs
+++ SlimeGG/Assets/Scripts/Dungeons/Managers/DungeonManager.cs
@@ -6,17 +6,28 @@ public class DungeonManager : MonoBehaviour
 {
     [SerializeField]
     StageController initStage;
+    [SerializeField]
+    Transform mainCamera;
 
     int[] curPos = new int[] { 0, 0 };
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(focusCamera());
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private IEnumerator focusCamera()
+    {
+        while (Mathf.Abs(mainCamera.position.x - initStage.transform.position.x) > 0.1f)
+        {
+            mainCamera.Translate(Vector3.left * (mainCamera.position.x - initStage.transform.position.x) * Time.deltaTime * 2f);
+            yield return new WaitForSeconds(0f);
+        }
     }
 }
