674d01383129a7fe623cda6764ac894cebba3352
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-29 22:07:50 +0900

    Add: User Token
    #50
    
    + 유저 현재 위치용 토큰 추가

--- SlimeGG/Assets/Scripts/Dungeons/Managers/DungeonManager.cs
+++ SlimeGG/Assets/Scripts/Dungeons/Managers/DungeonManager.cs
@@ -8,11 +8,14 @@ public class DungeonManager : MonoBehaviour
     StageController initStage;
     [SerializeField]
     Transform mainCamera;
+    [SerializeField]
+    Transform userTf;
 
     int[] curPos = new int[] { 0, 0 };
     // Start is called before the first frame update
     void Start()
     {
+        setUser();
         StartCoroutine(focusCamera());
     }
 
@@ -26,8 +29,13 @@ public class DungeonManager : MonoBehaviour
     {
         while (Mathf.Abs(mainCamera.position.x - initStage.transform.position.x) > 0.1f)
         {
-            mainCamera.Translate(Vector3.left * (mainCamera.position.x - initStage.transform.position.x) * Time.deltaTime * 2f);
+            mainCamera.Translate(Vector3.left * (mainCamera.position.x - initStage.transform.position.x) * Time.deltaTime * 2.5f);
             yield return new WaitForSeconds(0f);
         }
+    }
+
+    private void setUser()
+    {
+        userTf.position = initStage.transform.position;
     }
 }
