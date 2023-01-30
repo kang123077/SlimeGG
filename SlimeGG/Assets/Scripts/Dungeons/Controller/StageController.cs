627e4e0628794e5d129cc843818449387cf5c5c3
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-30 13:14:13 +0900

    Add: line between stages
    #50
    
    + 스테이지 사이를 잇는 선들 코드로 생성
    - 선 프리펩 추가
    - 예시 맵 삭제 <- 코드로 생성 가능

--- SlimeGG/Assets/Scripts/Dungeons/Controller/StageController.cs
+++ SlimeGG/Assets/Scripts/Dungeons/Controller/StageController.cs
@@ -8,17 +8,35 @@ public class StageController : MonoBehaviour
     StageController[] nextStageList;
     public bool isClear { get; set; }
     SpriteRenderer bgSprite;
+    Transform lineListTf;
+    [SerializeField]
+    Transform linePrefab;
+    Transform[] lineList;
 
     // Start is called before the first frame update
     void Start()
     {
         bgSprite = GetComponent<SpriteRenderer>();
         bgSprite.color = new Color(0.4f, 0.4f, 0.4f, 1);
+        lineListTf = transform.Find("Lines");
+
+        lineList = new Transform[nextStageList.Length];
+        for (int i = 0; i < lineList.Length; i++)
+        {
+            Transform newLineTf = Instantiate(linePrefab);
+            newLineTf.SetParent(lineListTf);
+            newLineTf.localPosition = Vector3.zero;
+            newLineTf.GetComponent<LineRenderer>().SetPositions(new Vector3[]
+            {
+                (nextStageList[i].transform.localPosition - transform.localPosition) * 0.1f,
+                (nextStageList[i].transform.localPosition - transform.localPosition) * 0.9f
+            });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
