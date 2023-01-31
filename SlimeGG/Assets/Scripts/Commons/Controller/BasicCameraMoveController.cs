b4a6260eea1589599e3581aec8296d148a3c08d9
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-01-31 15:47:33 +0900

    50 map scene (#54)
    * Add: Basic Logic
    
    #50
    
    + 기본적인 던전 시스템 구조 짜는중
    
    * Add: initial camera movement
    
    #50
    
    + 씬 로드 시, 현재 스테이지 위치로 카메라 smooth 이동
    
    * Add: User Token
    
    #50
    
    + 유저 현재 위치용 토큰 추가
    
    * Add: Fade in for Dungeon
    
    #50
    
    + 던전 씬 로드 시 페이드 인 로직 구성
    
    * Add: line between stages
    
    #50
    
    + 스테이지 사이를 잇는 선들 코드로 생성
    - 선 프리펩 추가
    - 예시 맵 삭제 <- 코드로 생성 가능
    
    * Feat: Color of stages and lines
    
    #50
    
    ~ 스테이지의 종류에 따른 스테이지 및 연결선 색상 변경 <- Enum 활용
    + 카메라 드래그 이동 로직 구성중
    
    * Add: camera movement
    
    #50
    
    + 카메라 이동 기본 로직 구성
    
    * Add: click event for stage
    
    #50
    
    + 스테이지 클릭 이벤트 추적
    + 스테이지 종류 추가: Shop
    
    * Add: Move among stage
    
    #50
    
    + 기본적인 스테이지 간 이동 로직 구성
    
    * Add: enter stage into battle
    
    #50
    
    + 스테이지 클릭 시 배틀씬으로 진입
    
    * Add: json for information of journey
    
    #50
    
    + json을 활용한 스테이지 진척도 로드
    ~ 데이터 로드 타이밍 조정:: 배틀 씬 -> 메인 메뉴 씬으로
    
    * Recover: Json file for field
    
    #0
    
    + 필드 정보 json 복구

---
+++ SlimeGG/Assets/Scripts/Commons/Controller/BasicCameraMoveController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCameraMoveController : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 xLimitCoor;
+    float correctionCoor = -1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LocalStorage.IS_CAMERA_FREE && Input.GetMouseButton(0))
+        {
+            if (correctionCoor != -1000f)
+            {
+                float curMousePos = Camera.main.transform.position.x - Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+                float targetCoor = correctionCoor + curMousePos;
+                transform.position = new Vector3(
+                    targetCoor < xLimitCoor.x 
+                    ? xLimitCoor.x
+                    : targetCoor > xLimitCoor.y
+                    ? xLimitCoor.y
+                    : targetCoor
+                    , 0f, -10f);
+            }
+            correctionCoor = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        }
+        else if (correctionCoor != -1000f)
+        {
+            correctionCoor = -1000f;
+        }
+    }
+}
