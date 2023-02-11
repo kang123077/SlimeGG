bf6f7c78528e31146ca033a3f9385d1f7bfc0c29
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-02-11 23:27:04 +0900

    Add: Basic structure for Reward
    #81
    
    + 배틀 종료 후 리워드 창 띄우기
    + 크기 조절

--- SlimeGG/Assets/Scripts/Rewards/RewardController.cs
+++ SlimeGG/Assets/Scripts/Rewards/RewardController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class RewardController : MonoBehaviour
 {
+    [SerializeField]
+    private Transform slotPrefab, contentPrefab;
+
     private TextMeshProUGUI titleText;
     private ObjectMoveController objectMoveController;
+    private GridLayoutGroup slotGridLayout;
+    private int curSlotCnt;
     // Start is called before the first frame update
     void Start()
     {
         titleText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         titleText.text = $"";
         objectMoveController = GetComponent<ObjectMoveController>();
+        slotGridLayout = transform.GetChild(1).GetComponent<GridLayoutGroup>();
     }
 
     // Update is called once per frame
@@ -24,7 +31,34 @@ public class RewardController : MonoBehaviour
     public void openMonsterReward()
     {
         titleText.text = $"몬스터 보상";
+        generateSlots(3);
         objectMoveController.toggle();
+    }
+
+    private void generateSlots(int cnt)
+    {
+        curSlotCnt = cnt;
+        while (cnt > 0)
+        {
+            Transform newSlot = Instantiate(slotPrefab);
+            newSlot.SetParent(slotGridLayout.transform);
+            newSlot.localScale = Vector3.one;
+            cnt--;
+        }
+    }
+
+    private void truncateSlots()
+    {
+        while (curSlotCnt > 0)
+        {
+            Destroy(slotGridLayout.transform.GetChild(0).gameObject);
+            curSlotCnt--;
+        }
+    }
+
+    private void generateItem()
+    {
+
     }
 
     public void openInventoryReward()
@@ -36,5 +70,6 @@ public class RewardController : MonoBehaviour
     public void closeReward()
     {
         objectMoveController.toggle();
+        truncateSlots();
     }
 }
